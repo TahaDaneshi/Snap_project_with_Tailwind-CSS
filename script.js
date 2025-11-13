1defe79043920d2aaa8bbd9b85ef86b37748d69c
TahaDanehsi <mttahadaneshi1384mt@gmail.com>
2025-11-13 19:09:33 +0330

    add root files

---
+++ script.js
@@ -0,0 +1,44 @@
+// variabels
+
+const imagesSliderArray = [
+    "./img/slider/Club.jpg",
+    "./img/slider/Food.jpg",
+    "./img/slider/Market.jpg",
+]
+const slider = document.getElementById("slider")
+const controlsSliderArray = document.getElementsByClassName('controls-slider')
+
+let flagSliderImage = 0;
+
+// functions 
+
+function changeSpanActive() {
+    for (let i = 0; i < controlsSliderArray.length; i++) {
+        controlsSliderArray[i].classList.remove("bg-gray-700")
+        controlsSliderArray[i].classList.remove("bg-gray-500")
+        controlsSliderArray[i].classList.add("bg-gray-500")
+    }
+    controlsSliderArray[flagSliderImage].classList.add("bg-gray-700")
+}
+
+setInterval(() => {
+    flagSliderImage++
+
+    if (flagSliderImage >= 3) { flagSliderImage = 0 }
+
+    changeSpanActive()
+
+    slider.setAttribute("src", imagesSliderArray[flagSliderImage])
+}, 3000)
+
+// Events
+
+for (let i of controlsSliderArray) {
+    i.addEventListener("click", (i) => {
+
+        flagSliderImage = i.target.dataset.number
+        slider.setAttribute("src", imagesSliderArray[i.target.dataset.number])
+
+        changeSpanActive()
+    })
+}
