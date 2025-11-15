872d360be4193c641251ca2ae517c673d4ebe779
TahaDanehsi <mttahadaneshi1384mt@gmail.com>
2025-11-15 10:19:26 +0330

    add: slider images and fixed some errors

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // variabels
 
 const imagesSliderArray = [
-    "./img/slider/Club.jpg",
-    "./img/slider/Food.jpg",
-    "./img/slider/Market.jpg",
+  "./src/img/slider/Club.jpg",
+  "./src/img/slider/Food.jpg",
+  "./src/img/slider/Market.jpg",
 ]
 const slider = document.getElementById("slider")
 const controlsSliderArray = document.getElementsByClassName('controls-slider')
@@ -13,32 +13,32 @@ let flagSliderImage = 0;
 // functions 
 
 function changeSpanActive() {
-    for (let i = 0; i < controlsSliderArray.length; i++) {
-        controlsSliderArray[i].classList.remove("bg-gray-700")
-        controlsSliderArray[i].classList.remove("bg-gray-500")
-        controlsSliderArray[i].classList.add("bg-gray-500")
-    }
-    controlsSliderArray[flagSliderImage].classList.add("bg-gray-700")
+  for (let i = 0; i < controlsSliderArray.length; i++) {
+    controlsSliderArray[i].classList.remove("bg-gray-700")
+    controlsSliderArray[i].classList.remove("bg-gray-500")
+    controlsSliderArray[i].classList.add("bg-gray-500")
+  }
+  controlsSliderArray[flagSliderImage].classList.add("bg-gray-700")
 }
 
 setInterval(() => {
-    flagSliderImage++
+  flagSliderImage++
 
-    if (flagSliderImage >= 3) { flagSliderImage = 0 }
+  if (flagSliderImage >= 3) { flagSliderImage = 0 }
 
-    changeSpanActive()
+  changeSpanActive()
 
-    slider.setAttribute("src", imagesSliderArray[flagSliderImage])
+  slider.setAttribute("src", imagesSliderArray[flagSliderImage])
 }, 3000)
 
 // Events
 
 for (let i of controlsSliderArray) {
-    i.addEventListener("click", (i) => {
+  i.addEventListener("click", (i) => {
 
-        flagSliderImage = i.target.dataset.number
-        slider.setAttribute("src", imagesSliderArray[i.target.dataset.number])
+    flagSliderImage = i.target.dataset.number
+    slider.setAttribute("src", imagesSliderArray[i.target.dataset.number])
 
-        changeSpanActive()
-    })
+    changeSpanActive()
+  })
 }
