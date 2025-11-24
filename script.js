160541ab23ac2e09b13689c6521a3b18fb771c77
TahaDanehsi <mttahadaneshi1384mt@gmail.com>
2025-11-24 08:01:56 +0330

    press size github icon

--- script.js
+++ script.js
@@ -9,16 +9,33 @@ const slider = document.getElementById("slider")
 const controlsSliderArray = document.getElementsByClassName('controls-slider')
 const darkMode = document.getElementsByClassName("darkMode");
 const modeIcons = [
-  ``,
+  `<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+>
+  <path d="M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+  <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+</svg>`,
   `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>`
 ]
 let flagSliderImage = 0;
 
 // functions 
 
+
+// Change Mode
+let flagMode = 0;
 function changeDarkMode() {
+  document.documentElement.classList.toggle("dark");
   for (i = 0; i < darkMode.length; i++) {
-    darkMode[i].innerHTML = modeIcons[1];
+    darkMode[i].innerHTML = modeIcons[flagMode];
   }
 
 }
