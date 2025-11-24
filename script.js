0786badbe07a97772f21481fd37edec99ad973b9
TahaDanehsi <mttahadaneshi1384mt@gmail.com>
2025-11-24 07:53:25 +0330

    add moon icone in dark mode

--- script.js
+++ script.js
@@ -7,10 +7,22 @@ const imagesSliderArray = [
 ]
 const slider = document.getElementById("slider")
 const controlsSliderArray = document.getElementsByClassName('controls-slider')
-
+const darkMode = document.getElementsByClassName("darkMode");
+const modeIcons = [
+  ``,
+  `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/></svg>`
+]
 let flagSliderImage = 0;
 
 // functions 
+
+function changeDarkMode() {
+  for (i = 0; i < darkMode.length; i++) {
+    darkMode[i].innerHTML = modeIcons[1];
+  }
+
+}
+changeDarkMode();
 
 function changeSpanActive() {
   for (let i = 0; i < controlsSliderArray.length; i++) {
